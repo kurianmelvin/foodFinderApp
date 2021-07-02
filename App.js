aaf4b12baacfd9a651a14b6e19db1bc6cb68ec88
Melvin Kurian <kurianmelvin@gmail.com>
2021-07-02 16:47:28 -0500

    testing prittier

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on MealsToGo app!</Text>
-      <StatusBar style="auto" />
+    <View>
+
+      <Text> this is MealsTOGo app</Text>
+
     </View>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
