0ca6100f4561c50c4e685fd0fb4cf1b215e08d95
Melvin Kurian <kurianmelvin@gmail.com>
2021-07-08 11:33:33 -0500

    initial resturant setup

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+
+import { RestaurantsScreen } from "../MealsToGo/src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
-    <View>
-      <Text> this is MealsTOGo app</Text>
-    </View>
+    <>
+      <RestaurantsScreen />
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
-
-const styles = StyleSheet.create({});
